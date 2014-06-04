bc8e874e329f3ec3e4512ec35de57196dafba190
George Yong <geoyws@gmail.com>
2014-06-04 23:08:54 +0800

    Wanted to go famo.us but then changed back to JQM

--- coderepo/coderepo.js
+++ coderepo/coderepo.js
@@ -1,4 +1,12 @@
-﻿//Web Service
-"/?type=message&target=musamansor"
-
-//Write a click listener that listens to what is clicked
+﻿$(document).on("pagecreate", "body", function () {
+    $(document).on("swipeleft swiperight", "body", function (event) {
+        if ($(".ui-page-active").jqmData("panel") !== "open") {
+            if (event.type === "swipeleft") {
+                $("#right-panel").panel("open");
+            }
+            else if (event.type === "swiperight") {
+                $("#left-panel").panel("open");
+            }
+        }
+    });
+});
