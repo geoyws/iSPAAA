2de02ddf1057b4d58a23296eb16c6d6560832f1e
George Yong <geoyws@gmail.com>
2014-06-06 01:30:17 +0800

    JQM panels added

--- js/ISPAAA.js
+++ js/ISPAAA.js
@@ -1,1 +1,17 @@
-﻿
+﻿$(function () {
+    $("div[data-role='panel']").panel().enhanceWithin();
+    //$("div[data-role='panel']").trigger("updatelayout");
+});
+
+$(document).on("pagecreate", "body", function () {
+    $(document).on("swipeleft swiperight", "body", function (event) {
+        if ($(".ui-page-active").jqmData("panel") !== "open") {
+            if (event.type === "swipeleft") {
+                $("#right-panel").panel("open");
+            }
+            else if (event.type === "swiperight") {
+                $("#left-panel").panel("open");
+            }
+        }
+    });
+});
